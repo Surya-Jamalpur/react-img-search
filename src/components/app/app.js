c8654b119651540e23d15ad7f08d4537706fee7a
Surya Jamalpuri <SJamalpuri@ad.prolifics.com>
2021-10-27 14:14:32 +0530

    added css cdn and created app component

---
+++ src/components/app/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class App extends React.Component{
+
+    render() {
+        return (
+            <p>Hi.. Am App Component</p>
+        )
+    }
+}
+export default App;
